d13825e8d39ea779811c61a004da259f0dddc71d
Richard Warburton <richard.warburton@gmail.com>
2015-01-29 14:47:41 +0000

    add comments to the sample main application

--- fix-gateway-samples/src/main/java/uk/co/real_logic/generated_api/SampleMain.java
+++ fix-gateway-samples/src/main/java/uk/co/real_logic/generated_api/SampleMain.java
@@ -33,29 +33,44 @@ public class SampleMain
 {
     public static void main(String[] args) throws Exception
     {
+        // Static configuration lasts the duration of a FIX-Gateway instance
         final StaticConfiguration configuration = new StaticConfiguration();
+
+        // You register the acceptor - which is your custom application hook
+        // that receives messages from external connections.
         configuration.registerAcceptor(new SampleAcceptor());
 
         try(final FixGateway gateway = FixGateway.launch(configuration))
         {
+            // The data dictionary is generated from the standard FIX XML dictionary specification.
             final DataDictionary dictionary = new DataDictionary();
 
+            // Each outbound session with an Exchange or broker is represented by
+            // a Session object. Each session object can be configured with connection
+            // details and credentials.
             final SessionConfiguration sessionConfig = new SessionConfiguration()
                     .address("broker.example.com", 9999)
                     .credentials("username", "password");
 
             final Session session = gateway.initiate(sessionConfig, dictionary);
 
+            // Specific encoders are generated for each type of message
+            // from the same dictionary as the decoders.
             final OrderSingleEncoder orderSingle = dictionary.newOrderSingleEncoder();
             orderSingle.clOrdID("1");
             orderSingle.handlInst('1');
             orderSingle.ordType(Market);
+            // The API would follow a fluent style for setting up the different FIX message fields.
             orderSingle.side(Buy);
             orderSingle.symbol("MSFT");
             orderSingle.transactTime(System.currentTimeMillis());
 
+            // Having encoded the message, you can send it to the exchange via the session object.
             session.send(orderSingle);
 
+            // If you want to produce multiple messages and rapidly fire them off then you just
+            // need to update the fields in question and the other remain the side as your previous
+            // usage.
             orderSingle.side(Sell);
             session.send(orderSingle);
         }
