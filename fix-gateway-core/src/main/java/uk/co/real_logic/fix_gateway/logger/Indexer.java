2b239e3cf0cf822c81fa6d9d915d3b93491b1e1b
Richard Warburton <richard.warburton@gmail.com>
2015-05-07 18:16:18 +0100

    implement a simple subscription based indexer

--- fix-gateway-core/src/main/java/uk/co/real_logic/fix_gateway/logger/Indexer.java
+++ fix-gateway-core/src/main/java/uk/co/real_logic/fix_gateway/logger/Indexer.java
@@ -15,24 +15,39 @@
  */
 package uk.co.real_logic.fix_gateway.logger;
 
+import uk.co.real_logic.aeron.Subscription;
+import uk.co.real_logic.aeron.common.concurrent.logbuffer.DataHandler;
+import uk.co.real_logic.aeron.common.concurrent.logbuffer.Header;
+import uk.co.real_logic.agrona.DirectBuffer;
 import uk.co.real_logic.agrona.concurrent.Agent;
 
 import java.util.List;
 
-public class Indexer implements Agent
+public class Indexer implements Agent, DataHandler
 {
+    private static final int LIMIT = 10;
 
     private final List<Index> indices;
+    private final Subscription subscription;
 
-    public Indexer(final List<Index> indices)
+    public Indexer(final List<Index> indices, final Subscription subscription)
     {
         this.indices = indices;
+        this.subscription = subscription;
     }
 
     public int doWork() throws Exception
     {
-        // TODO: read messages out of the log file
-        return 0;
+        return subscription.poll(LIMIT);
+    }
+
+    @Override
+    public void onData(DirectBuffer buffer, int offset, int length, Header header)
+    {
+        for (final Index index : indices)
+        {
+            index.indexRecord(buffer, offset, length, header.streamId());
+        }
     }
 
     public void onClose()
