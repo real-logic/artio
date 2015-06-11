439268670ef9ec80910685d43028348ec1f0b74d
Richard Warburton <richard.warburton@gmail.com>
2015-06-11 14:07:51 +0100

    remove the marketcetera repository and quick integration and system tests

--- fix-gateway-system-tests/src/test/java/uk/co/real_logic/fix_gateway/system_tests/SystemTestUtil.java
+++ fix-gateway-system-tests/src/test/java/uk/co/real_logic/fix_gateway/system_tests/SystemTestUtil.java
@@ -1,14 +1,23 @@
+/*
+ * Copyright 2015 Real Logic Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package uk.co.real_logic.fix_gateway.system_tests;
 
 import org.hamcrest.Matcher;
-import quickfix.*;
-import quickfix.field.BeginString;
-import quickfix.field.MsgType;
-import quickfix.field.SenderCompID;
-import quickfix.field.TargetCompID;
 import uk.co.real_logic.aeron.driver.MediaDriver;
 import uk.co.real_logic.agrona.IoUtil;
-import uk.co.real_logic.fix_gateway.DebugLogger;
 import uk.co.real_logic.fix_gateway.FixGateway;
 import uk.co.real_logic.fix_gateway.SessionConfiguration;
 import uk.co.real_logic.fix_gateway.StaticConfiguration;
@@ -23,12 +32,10 @@ import uk.co.real_logic.fix_gateway.session.Session;
 
 import java.io.File;
 import java.util.Arrays;
-import java.util.List;
 
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
-import static quickfix.field.MsgType.TEST_REQUEST;
 import static uk.co.real_logic.aeron.driver.ThreadingMode.SHARED;
 import static uk.co.real_logic.fix_gateway.TestFixtures.unusedPort;
 import static uk.co.real_logic.fix_gateway.Timing.assertEventuallyEquals;
@@ -84,16 +91,6 @@ public final class SystemTestUtil
         assertEventuallyEquals("Failed to receive a logon and test request message", 2, sessionHandler::poll);
         assertEquals(2, acceptor.messageTypes().size());
         assertThat(acceptor.messageTypes(), hasItem(TestRequestDecoder.MESSAGE_TYPE));
-    }
-
-    public static void assertQuickFixDisconnected(
-        final FakeQuickFixApplication acceptor,
-        final Matcher<Iterable<? super SessionID>> sessionMatcher)
-    {
-        assertEventuallyEquals("Failed to receive a logout", 1, () -> acceptor.logouts().size());
-        final List<SessionID> logouts = acceptor.logouts();
-        DebugLogger.log("\nLogouts: %s\n", logouts);
-        assertThat(logouts, sessionMatcher);
     }
 
     public static <T> Matcher<Iterable<? super T>> containsInitiator()
@@ -168,93 +165,4 @@ public final class SystemTestUtil
         return FixGateway.launch(acceptingConfig);
     }
 
-    public static SocketAcceptor launchQuickFixAcceptor(final int port, final FakeQuickFixApplication application)
-        throws ConfigError
-    {
-        final SessionID sessionID = sessionID(ACCEPTOR_ID, INITIATOR_ID);
-        final SessionSettings settings = sessionSettings(sessionID);
-
-        settings.setString("SocketAcceptPort", String.valueOf(port));
-        settings.setString(sessionID, "ConnectionType", "acceptor");
-
-        final FileStoreFactory storeFactory = new FileStoreFactory(settings);
-        final LogFactory logFactory = new ScreenLogFactory(settings);
-        final SocketAcceptor socketAcceptor = new SocketAcceptor(
-            application, storeFactory, settings, logFactory, new DefaultMessageFactory());
-        socketAcceptor.start();
-
-        return socketAcceptor;
-    }
-
-    public static SocketInitiator launchQuickFixInitiator(
-        final int port, final FakeQuickFixApplication application) throws ConfigError
-    {
-        final SessionID sessionID = sessionID(INITIATOR_ID, ACCEPTOR_ID);
-        final SessionSettings settings = sessionSettings(sessionID);
-
-        settings.setString("HeartBtInt", "30");
-
-        settings.setString(sessionID, "ConnectionType", "initiator");
-        settings.setString(sessionID, "SocketConnectPort", String.valueOf(port));
-        settings.setString(sessionID, "SocketConnectHost", "localhost");
-
-        final FileStoreFactory storeFactory = new FileStoreFactory(settings);
-        final LogFactory logFactory = new ScreenLogFactory(settings);
-        final SocketInitiator socketInitiator = new SocketInitiator(
-            application, storeFactory, settings, logFactory, new DefaultMessageFactory());
-        socketInitiator.start();
-        return socketInitiator;
-    }
-
-    private static SessionSettings sessionSettings(final SessionID sessionID)
-    {
-        final SessionSettings settings = new SessionSettings();
-        final String path = "build/tmp/quickfix";
-        IoUtil.delete(new File(path), true);
-        settings.setString("FileStorePath", path);
-        settings.setString("DataDictionary", "FIX44.xml");
-        settings.setString("BeginString", "FIX.4.4");
-        settings.setString(sessionID, "StartTime", "00:00:00");
-        settings.setString(sessionID, "EndTime", "00:00:00");
-        return settings;
-    }
-
-    private static SessionID sessionID(final String senderCompId, final String targetCompId)
-    {
-        return new SessionID(
-            new BeginString("FIX.4.4"),
-            new SenderCompID(senderCompId),
-            new TargetCompID(targetCompId)
-        );
-    }
-
-    static void assertQuickFixReceivedMessage(final FakeQuickFixApplication acceptor)
-    {
-        assertEventuallyTrue("Unable to fnd test request", () ->
-        {
-            final List<Message> messages = acceptor.messages();
-            for (final Message message : messages)
-            {
-                if (TEST_REQUEST.equals(getMsgType(message)))
-                {
-                    return true;
-                }
-            }
-
-            return false;
-        });
-    }
-
-    private static String getMsgType(final Message message)
-    {
-        try
-        {
-            return message.getHeader().getField(new MsgType()).getValue();
-        }
-        catch (final FieldNotFound ex)
-        {
-            ex.printStackTrace();
-            return null;
-        }
-    }
 }
