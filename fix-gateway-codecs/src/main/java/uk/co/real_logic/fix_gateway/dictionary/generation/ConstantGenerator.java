33b338c3b7eeed180ef12599ec5597da9287421d
Richard Warburton <richard.warburton@gmail.com>
2015-07-09 15:34:38 +0100

    add code to generate constants

--- fix-gateway-codecs/src/main/java/uk/co/real_logic/fix_gateway/dictionary/generation/ConstantGenerator.java
+++ fix-gateway-codecs/src/main/java/uk/co/real_logic/fix_gateway/dictionary/generation/ConstantGenerator.java
@@ -15,13 +15,18 @@
  */
 package uk.co.real_logic.fix_gateway.dictionary.generation;
 
+import uk.co.real_logic.agrona.collections.IntHashSet;
 import uk.co.real_logic.agrona.generation.OutputManager;
 import uk.co.real_logic.fix_gateway.dictionary.ir.Dictionary;
+import uk.co.real_logic.fix_gateway.dictionary.ir.Field;
+
+import java.util.stream.Stream;
 
 import static java.lang.Character.isUpperCase;
 import static java.lang.Character.toUpperCase;
 import static java.util.stream.Collectors.joining;
 import static uk.co.real_logic.fix_gateway.dictionary.generation.GenerationUtil.fileHeader;
+import static uk.co.real_logic.fix_gateway.dictionary.generation.GenerationUtil.importFor;
 
 public class ConstantGenerator
 {
@@ -29,6 +34,7 @@ public class ConstantGenerator
 
     private static final String BODY =
         "public class " + CLASS_NAME + "\n" + "{\n\n";
+    public static final String ALL_FIELDS = "ALL_FIELDS";
 
     private final Dictionary dictionary;
     private final String builderPackage;
@@ -47,11 +53,38 @@ public class ConstantGenerator
         outputManager.withOutput(CLASS_NAME, out ->
         {
             out.append(fileHeader(builderPackage));
+            out.append(importFor(IntHashSet.class));
             out.append(BODY);
             out.append(generateMessageTypes());
             out.append(generateFieldTags());
+            out.append(generateFieldDictionary());
             out.append("}\n");
         });
+    }
+
+    private String generateFieldDictionary()
+    {
+        final String addFields =
+            fields().map(this::addField)
+                    .collect(joining());
+        final int hashMapSize = (int) (((double) fields().count()) / 0.6);
+        return String.format(
+            "    public static final IntHashSet " + ALL_FIELDS + " = new IntHashSet(%1$d, -1);\n\n" +
+            "    static\n" +
+            "    {\n" +
+            "%2$s" +
+            "    }\n\n",
+            hashMapSize,
+            addFields);
+    }
+
+    private String addField(final Field field)
+    {
+        return String.format(
+            "        %1$s.add(%2$d);\n",
+            ALL_FIELDS,
+            field.number()
+        );
     }
 
     private String generateMessageTypes()
@@ -65,12 +98,17 @@ public class ConstantGenerator
 
     private String generateFieldTags()
     {
+        return fields()
+            .map(field -> generateIntConstant(field.name(), field.number()))
+            .collect(joining());
+    }
+
+    private Stream<Field> fields()
+    {
         return dictionary
             .fields()
             .values()
-            .stream()
-            .map(field -> generateIntConstant(field.name(), field.number()))
-            .collect(joining());
+            .stream();
     }
 
     private String generateIntConstant(final String name, final int number)
--- fix-gateway-codecs/src/test/java/uk/co/real_logic/fix_gateway/dictionary/generation/ConstantGeneratorTest.java
+++ fix-gateway-codecs/src/test/java/uk/co/real_logic/fix_gateway/dictionary/generation/ConstantGeneratorTest.java
@@ -17,14 +17,19 @@ package uk.co.real_logic.fix_gateway.dictionary.generation;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
+import uk.co.real_logic.agrona.collections.IntHashSet;
 import uk.co.real_logic.agrona.generation.StringWriterOutputManager;
-import uk.co.real_logic.fix_gateway.util.Reflection;
 
 import java.util.Map;
+import java.util.Set;
 
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 import static uk.co.real_logic.agrona.generation.CompilerUtil.compileInMemory;
 import static uk.co.real_logic.fix_gateway.dictionary.ExampleDictionary.*;
+import static uk.co.real_logic.fix_gateway.dictionary.generation.ConstantGenerator.ALL_FIELDS;
+import static uk.co.real_logic.fix_gateway.util.Reflection.getField;
 
 public class ConstantGeneratorTest
 {
@@ -48,13 +53,27 @@ public class ConstantGeneratorTest
     @Test
     public void shouldContainNumericConstantsForFieldTags() throws Exception
     {
-        assertEquals(TEST_REQ_ID_TAG, Reflection.getField(constants, "TEST_REQ_ID"));
+        assertEquals(TEST_REQ_ID_TAG, getField(constants, "TEST_REQ_ID"));
     }
 
     @Test
     public void shouldContainNumericConstantsForMessageTypes() throws Exception
     {
-        assertEquals(HEARTBEAT_TYPE, Reflection.getField(constants, "HEARTBEAT"));
+        assertEquals(HEARTBEAT_TYPE, getField(constants, "HEARTBEAT"));
+    }
+
+    @Test
+    public void shouldEncodeAllFieldsSet() throws Exception
+    {
+        final Object allFieldsField = getField(constants, ALL_FIELDS);
+        assertThat(allFieldsField, instanceOf(IntHashSet.class));
+
+        @SuppressWarnings("unchecked")
+        final Set<Integer> allFields = (Set<Integer>) allFieldsField;
+        assertThat(allFields, hasItem(123));
+        assertThat(allFields, hasItem(124));
+        assertThat(allFields, hasItem(35));
+        assertThat(allFields, not(hasItem(999)));
     }
 
 }
