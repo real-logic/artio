8cbe61c7ebf45a2d8a2f0e8e06ad0ac87bf5a50a
Richard Warburton <richard.warburton@gmail.com>
2015-08-24 15:30:33 +0100

    add the sender and target, sub and location fields to the session dictionary and a session id strategy for (SenderCompID, SenderSubID,TargetCompID)

--- fix-gateway-core/src/main/java/uk/co/real_logic/fix_gateway/session/SenderTargetAndSubSessionIdStrategy.java
+++ fix-gateway-core/src/main/java/uk/co/real_logic/fix_gateway/session/SenderTargetAndSubSessionIdStrategy.java
@@ -26,43 +26,50 @@ import java.util.Arrays;
  *
  * Threadsafe - no state;
  */
-public class SenderAndTargetSessionIdStrategy implements SessionIdStrategy
+public class SenderTargetAndSubSessionIdStrategy implements SessionIdStrategy
 {
     public Object onAcceptorLogon(final HeaderDecoder header)
     {
-        return new CompositeKey(header.targetCompID(), header.senderCompID());
+        final char[] senderCompID = Arrays.copyOf(header.targetCompID(), header.targetCompIDLength());
+        final char[] senderSubID = Arrays.copyOf(header.targetSubID(), header.targetSubIDLength());
+        final char[] targetCompId = Arrays.copyOf(header.senderCompID(), header.senderCompIDLength());
+        return new CompositeKey(senderCompID, senderSubID, targetCompId);
     }
 
     public Object onInitiatorLogon(
         final String senderCompId, final String senderSubId, final String senderLocationId, final String targetCompId)
     {
-        return new CompositeKey(senderCompId.toCharArray(), targetCompId.toCharArray());
+        return new CompositeKey(senderCompId.toCharArray(), senderSubId.toCharArray(), targetCompId.toCharArray());
     }
 
     public void setupSession(final Object compositeKey, final HeaderEncoder encoder)
     {
-        final CompositeKey composite = (CompositeKey)compositeKey;
+        final CompositeKey composite = (CompositeKey) compositeKey;
         encoder.senderCompID(composite.senderCompID);
+        encoder.senderSubID(composite.senderSubID);
         encoder.targetCompID(composite.targetCompID);
     }
 
     private static final class CompositeKey
     {
         private final char[] senderCompID;
+        private final char[] senderSubID;
         private final char[] targetCompID;
         private final int hashCode;
 
-        private CompositeKey(final char[] senderCompID, final char[] targetCompID)
+        private CompositeKey(final char[] senderCompID, final char[] senderSubID, final char[] targetCompID)
         {
             this.senderCompID = senderCompID;
+            this.senderSubID = senderSubID;
             this.targetCompID = targetCompID;
 
-            hashCode = hash(senderCompID, targetCompID);
+            hashCode = hash(senderCompID, senderSubID, targetCompID);
         }
 
-        private int hash(final char[] senderCompID, final char[] targetCompID)
+        private int hash(final char[] senderCompID, final char[] senderSubID, final char[] targetCompID)
         {
             int result  = Arrays.hashCode(senderCompID);
+            result = 31 * result + Arrays.hashCode(senderSubID);
             result = 31 * result + Arrays.hashCode(targetCompID);
             return result;
         }
@@ -78,6 +85,7 @@ public class SenderAndTargetSessionIdStrategy implements SessionIdStrategy
             {
                 final CompositeKey compositeKey = (CompositeKey)obj;
                 return Arrays.equals(compositeKey.senderCompID, senderCompID)
+                    && Arrays.equals(compositeKey.senderSubID, senderSubID)
                     && Arrays.equals(compositeKey.targetCompID, targetCompID);
             }
 
@@ -88,6 +96,7 @@ public class SenderAndTargetSessionIdStrategy implements SessionIdStrategy
         {
             return "CompositeKey{" +
                 "senderCompID=" + Arrays.toString(senderCompID) +
+                "senderSubID=" + Arrays.toString(senderSubID) +
                 ", targetCompID=" + Arrays.toString(targetCompID) +
                 '}';
         }
--- fix-gateway-core/src/test/java/uk/co/real_logic/fix_gateway/session/SenderTargetAndSubSessionIdStrategyTest.java
+++ fix-gateway-core/src/test/java/uk/co/real_logic/fix_gateway/session/SenderTargetAndSubSessionIdStrategyTest.java
@@ -17,44 +17,43 @@ package uk.co.real_logic.fix_gateway.session;
 
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Set;
 
 import static java.util.stream.Collectors.toSet;
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+import static uk.co.real_logic.fix_gateway.session.SenderAndTargetSessionIdStrategyTest.IDS;
 
-public class SenderAndTargetSessionIdStrategyTest
+public class SenderTargetAndSubSessionIdStrategyTest
 {
-    public static final List<String> IDS = Arrays.asList("SIGMAX", "ABC_DEFG04", "LEH_LZJ02");
 
-    private SenderAndTargetSessionIdStrategy strategy = new SenderAndTargetSessionIdStrategy();
+    private SenderTargetAndSubSessionIdStrategy strategy = new SenderTargetAndSubSessionIdStrategy();
 
     @Test
     public void differentIdsDoNotClash()
     {
-        final Set<Object> compositeKeys = IDS.stream()
-            .flatMap((sender) ->
-                IDS.stream()
-                   .map((target) -> strategy.onInitiatorLogon(sender, null, null, target)))
+        final Set<Object> compositeKeys =
+            IDS.stream().flatMap((sender) ->
+                IDS.stream().flatMap((senderSub) ->
+                    IDS.stream().map(
+                        (target) -> strategy.onInitiatorLogon(sender, senderSub, null, target))))
             .collect(toSet());
 
-        assertThat(compositeKeys, hasSize(IDS.size() * IDS.size()));
+        assertThat(compositeKeys, hasSize(IDS.size() * IDS.size() * IDS.size()));
     }
 
     @Test
     public void theSameIdIsEqual()
     {
         IDS.forEach((sender) ->
-            IDS.forEach((target) ->
-            {
-                final Object first = strategy.onInitiatorLogon(sender, null, null, target);
-                final Object second = strategy.onInitiatorLogon(sender, null, null, target);
-                assertEquals(first, second);
-                assertEquals(first.hashCode(), second.hashCode());
-            }));
+            IDS.forEach((senderSub) ->
+                IDS.forEach((target) ->
+                {
+                    final Object first = strategy.onInitiatorLogon(sender, senderSub, null, target);
+                    final Object second = strategy.onInitiatorLogon(sender, senderSub, null, target);
+                    assertEquals(first, second);
+                    assertEquals(first.hashCode(), second.hashCode());
+                })));
     }
-
 }
