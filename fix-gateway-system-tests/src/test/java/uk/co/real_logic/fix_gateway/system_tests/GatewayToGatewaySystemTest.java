51777fd2b6acd15dfbdc6cb0aa10a3705424e43c
Richard Warburton <richard.warburton@gmail.com>
2016-03-31 18:11:17 +0100

    unignore test

--- fix-gateway-system-tests/src/test/java/uk/co/real_logic/fix_gateway/system_tests/GatewayToGatewaySystemTest.java
+++ fix-gateway-system-tests/src/test/java/uk/co/real_logic/fix_gateway/system_tests/GatewayToGatewaySystemTest.java
@@ -16,7 +16,6 @@
 package uk.co.real_logic.fix_gateway.system_tests;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import uk.co.real_logic.fix_gateway.engine.FixEngine;
 import uk.co.real_logic.fix_gateway.engine.SessionInfo;
@@ -144,7 +143,6 @@ public class GatewayToGatewaySystemTest extends AbstractGatewayToGatewaySystemTe
         assertEquals(initiatingSession.connectedHost(), "localhost");
     }
 
-    @Ignore
     @Test
     public void multipleLibrariesCanExchangeMessages()
     {
