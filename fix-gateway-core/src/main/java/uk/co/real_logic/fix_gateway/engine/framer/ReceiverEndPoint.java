5728e646d9abd3937291177d9621c52d2cbda78a
Richard Warburton <richard.warburton@gmail.com>
2016-09-22 11:57:49 +0100

    fix memory leak

--- fix-gateway-core/src/main/java/uk/co/real_logic/fix_gateway/engine/framer/ReceiverEndPoint.java
+++ fix-gateway-core/src/main/java/uk/co/real_logic/fix_gateway/engine/framer/ReceiverEndPoint.java
@@ -333,6 +333,7 @@ class ReceiverEndPoint
                     return true;
                 }
                 close(DisconnectReason.DUPLICATE_SESSION);
+                removeEndpointFromFramer();
             }
             else
             {
