2ead2ed400f64bead39fb143fccfdea7023ad81a
Richard Warburton <richard.warburton@gmail.com>
2017-01-25 10:59:05 +0000

    Add debug logging

--- fix-gateway-core/src/main/scripts/cluster-repeat-test.sh
+++ fix-gateway-core/src/main/scripts/cluster-repeat-test.sh
@@ -5,6 +5,6 @@ count=$1
 for i in $(seq 1 $count)
 do
   echo $i
-  ./gradlew -Dtest.single=ClusterReplicationTest --rerun-tasks :fix-gateway-core:test > "$i.out"
+  ./gradlew -Dfix.core.debug=RAFT -Dtest.single=ClusterReplicationTest --rerun-tasks :fix-gateway-core:test > "$i.out"
 done
 
