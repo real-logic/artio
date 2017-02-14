49318e291d350ea04fd70ddff6bf8ca7826650f7
Richard Warburton <richard.warburton@gmail.com>
2017-02-14 17:38:37 +0000

    Fix benchmark printf bug

--- fix-gateway-system-tests/src/perf/java/uk/co/real_logic/fix_gateway/system_benchmarks/BenchmarkSessionHandler.java
+++ fix-gateway-system-tests/src/perf/java/uk/co/real_logic/fix_gateway/system_benchmarks/BenchmarkSessionHandler.java
@@ -49,7 +49,7 @@ public final class BenchmarkSessionHandler implements SessionHandler
 
     public Action onDisconnect(final int libraryId, final Session session, final DisconnectReason reason)
     {
-        System.out.printf("%d disconnected due to %s\n", session, reason);
+        System.out.printf("%d disconnected due to %s\n", session.id(), reason);
 
         return CONTINUE;
     }
