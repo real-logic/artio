318ab3b3bc47de67bc889071f7ba4222fb1d38a3
Richard Warburton <richard.warburton@gmail.com>
2017-04-10 14:10:19 +0100

    Fix formatting issue

--- fix-gateway-core/src/main/java/uk/co/real_logic/fix_gateway/engine/framer/EngineLibraryInfo.java
+++ fix-gateway-core/src/main/java/uk/co/real_logic/fix_gateway/engine/framer/EngineLibraryInfo.java
@@ -53,8 +53,14 @@ class EngineLibraryInfo implements LibraryInfo
     @Override
     public boolean equals(final Object o)
     {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o)
+        {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass())
+        {
+            return false;
+        }
 
         final EngineLibraryInfo that = (EngineLibraryInfo) o;
 
--- fix-gateway-core/src/main/java/uk/co/real_logic/fix_gateway/engine/framer/LiveLibraryInfo.java
+++ fix-gateway-core/src/main/java/uk/co/real_logic/fix_gateway/engine/framer/LiveLibraryInfo.java
@@ -117,8 +117,14 @@ final class LiveLibraryInfo implements LibraryInfo
     @Override
     public boolean equals(final Object o)
     {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o)
+        {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass())
+        {
+            return false;
+        }
 
         final LiveLibraryInfo that = (LiveLibraryInfo) o;
 
