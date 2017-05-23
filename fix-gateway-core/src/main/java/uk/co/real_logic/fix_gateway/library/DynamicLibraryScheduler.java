1205a97ed26239a294fcb83b78635e536bc592c1
Richard Warburton <richard.warburton@gmail.com>
2017-05-23 15:42:39 +0100

    Split out the DynamicCompositeAgent into its own class

--- fix-gateway-core/src/main/java/uk/co/real_logic/fix_gateway/library/DynamicLibraryScheduler.java
+++ fix-gateway-core/src/main/java/uk/co/real_logic/fix_gateway/library/DynamicLibraryScheduler.java
@@ -18,13 +18,9 @@ package uk.co.real_logic.fix_gateway.library;
 import io.aeron.Aeron;
 import org.agrona.CloseHelper;
 import org.agrona.ErrorHandler;
-import org.agrona.collections.ArrayUtil;
 import org.agrona.collections.Int2ObjectHashMap;
 import org.agrona.concurrent.Agent;
 import org.agrona.concurrent.AgentRunner;
-import org.agrona.concurrent.ManyToOneConcurrentArrayQueue;
-
-import java.util.Queue;
 
 /**
  * Share the monitoring thread over multiple instances of a library.
@@ -78,96 +74,4 @@ public class DynamicLibraryScheduler implements LibraryScheduler
             .useConductorAgentInvoker(true);
     }
 
-    private final class DynamicCompositeAgent implements Agent
-    {
-        private final Queue<Command> commandQueue = new ManyToOneConcurrentArrayQueue<>(10);
-        private final String roleName;
-
-        private Agent[] agents = new Agent[0];
-
-        private DynamicCompositeAgent(final String roleName)
-        {
-            this.roleName = roleName;
-        }
-
-        private void add(final Agent agent)
-        {
-            perform(new Command(agent, true));
-        }
-
-        private void remove(final Agent agent)
-        {
-            perform(new Command(agent, false));
-        }
-
-        private void perform(final Command command)
-        {
-            while (!commandQueue.offer(command))
-            {
-                Thread.yield();
-            }
-
-            Thread.yield();
-
-            while (!command.done)
-            {
-                Thread.yield();
-            }
-        }
-
-        public int doWork() throws Exception
-        {
-            int operations = pollCommands();
-
-            final Agent[] agents = this.agents;
-            for (int i = 0; i < agents.length; i++)
-            {
-                operations += agents[i].doWork();
-            }
-
-            return operations;
-        }
-
-        private int pollCommands()
-        {
-            final Command command = commandQueue.poll();
-            if (command != null)
-            {
-                if (command.isAdd)
-                {
-                    agents = ArrayUtil.add(agents, command.agent);
-                }
-                else
-                {
-                    agents = ArrayUtil.remove(agents, command.agent);
-
-                    command.agent.onClose();
-                }
-
-                command.done = true;
-
-                return 1;
-            }
-
-            return 0;
-        }
-
-        public String roleName()
-        {
-            return roleName;
-        }
-    }
-
-    private final class Command
-    {
-        private final Agent agent;
-        private final boolean isAdd;
-        private volatile boolean done = false;
-
-        private Command(final Agent agent, final boolean isAdd)
-        {
-            this.agent = agent;
-            this.isAdd = isAdd;
-        }
-    }
 }
