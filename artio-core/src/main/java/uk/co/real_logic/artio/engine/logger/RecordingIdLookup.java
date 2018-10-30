57ac24573d437ea1f0312a533b6d44440c94d143
Richard Warburton <richard.warburton@gmail.com>
2018-10-30 15:35:28 +0000

    update licensing headers

--- artio-core/src/main/java/uk/co/real_logic/artio/engine/logger/RecordingIdLookup.java
+++ artio-core/src/main/java/uk/co/real_logic/artio/engine/logger/RecordingIdLookup.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Adaptive Financial Consulting Ltd.
+ * Copyright 2015-2018 Real Logic Ltd, Adaptive Financial Consulting Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
