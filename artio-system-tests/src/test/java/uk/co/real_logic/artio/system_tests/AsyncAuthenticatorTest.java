86d6fbf6f33c76ec19f5df07ab22b3001e0b47bd
Richard Warburton <richard.warburton@gmail.com>
2019-04-25 14:37:48 +0100

    document the new async authentication strategy

--- artio-system-tests/src/test/java/uk/co/real_logic/artio/system_tests/AsyncAuthenticatorTest.java
+++ artio-system-tests/src/test/java/uk/co/real_logic/artio/system_tests/AsyncAuthenticatorTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2015-2019 Real Logic Ltd, Adaptive Financial Consulting Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package uk.co.real_logic.artio.system_tests;
 
 import org.junit.Before;
@@ -19,9 +34,9 @@ import static uk.co.real_logic.artio.system_tests.SystemTestUtil.*;
 
 public class AsyncAuthenticatorTest extends AbstractGatewayToGatewaySystemTest
 {
-    public static final int DEFAULT_TIMEOUT_IN_MS = 1_000;
+    private static final int DEFAULT_TIMEOUT_IN_MS = 1_000;
     private final FakeConnectHandler fakeConnectHandler = new FakeConnectHandler();
-    private final MyAuthenticationStrategy auth = new MyAuthenticationStrategy();
+    private final FakeAsyncAuthenticationStrategy auth = new FakeAsyncAuthenticationStrategy();
 
     @Before
     public void launch()
@@ -67,7 +82,7 @@ public class AsyncAuthenticatorTest extends AbstractGatewayToGatewaySystemTest
         auth.verifyNoBlockingCalls();
     }
 
-    private class MyAuthenticationStrategy implements AuthenticationStrategy
+    private class FakeAsyncAuthenticationStrategy implements AuthenticationStrategy
     {
         volatile boolean blockingAuthenticateCalled;
         private volatile AuthenticationProxy authProxy;
@@ -84,7 +99,7 @@ public class AsyncAuthenticatorTest extends AbstractGatewayToGatewaySystemTest
             throw new UnsupportedOperationException();
         }
 
-        public void verifyNoBlockingCalls()
+        void verifyNoBlockingCalls()
         {
             assertFalse(blockingAuthenticateCalled);
         }
