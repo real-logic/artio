f8677489cdadd79b95fabe0b6b754e03b18b85d6
Richard Warburton <richard.warburton@gmail.com>
2019-05-28 15:52:08 +0100

    actually add the deprecated annotation

--- artio-codecs/src/main/java/uk/co/real_logic/artio/fields/UtcTimestampEncoder.java
+++ artio-codecs/src/main/java/uk/co/real_logic/artio/fields/UtcTimestampEncoder.java
@@ -69,6 +69,7 @@ public final class UtcTimestampEncoder
      *                                        timeunit for the <code>epochFraction</code> passed to encode().
      *                                        False if you wish to use microseconds.
      */
+    @Deprecated
     public UtcTimestampEncoder(final boolean usesMillisecondsAsEpochFraction)
     {
         this(usesMillisecondsAsEpochFraction ?
