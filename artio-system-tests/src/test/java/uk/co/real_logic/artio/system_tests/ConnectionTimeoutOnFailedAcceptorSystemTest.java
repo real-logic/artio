700c6cc7c6ecb53cadd414e946b64e159550124c
AmitA396 <56270937+AmitA396@users.noreply.github.com>
2019-10-14 11:18:58 -0400

    Create ConnectionTimeoutOnFailedAcceptorSystemTest.java
    Test to prove that client goes in a loop and does not come out of test.

---
+++ artio-system-tests/src/test/java/uk/co/real_logic/artio/system_tests/ConnectionTimeoutOnFailedAcceptorSystemTest.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2015-2018 Real Logic Ltd, Adaptive Financial Consulting Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.real_logic.artio.system_tests;
+
+import org.junit.Before;
+import org.junit.Test;
+import uk.co.real_logic.artio.Reply;
+import uk.co.real_logic.artio.engine.EngineConfiguration;
+import uk.co.real_logic.artio.engine.FixEngine;
+import uk.co.real_logic.artio.library.LibraryConfiguration;
+import uk.co.real_logic.artio.library.SessionConfiguration;
+import uk.co.real_logic.artio.session.Session;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import static org.junit.Assert.assertEquals;
+import static uk.co.real_logic.artio.TestFixtures.launchMediaDriver;
+import static uk.co.real_logic.artio.system_tests.SystemTestUtil.ACCEPTOR_ID;
+import static uk.co.real_logic.artio.system_tests.SystemTestUtil.ACCEPTOR_LOGS;
+import static uk.co.real_logic.artio.system_tests.SystemTestUtil.CLIENT_LOGS;
+import static uk.co.real_logic.artio.system_tests.SystemTestUtil.INITIATOR_ID;
+import static uk.co.real_logic.artio.system_tests.SystemTestUtil.PASSWORD;
+import static uk.co.real_logic.artio.system_tests.SystemTestUtil.USERNAME;
+import static uk.co.real_logic.artio.system_tests.SystemTestUtil.acceptingLibraryConfig;
+import static uk.co.real_logic.artio.system_tests.SystemTestUtil.delete;
+import static uk.co.real_logic.artio.system_tests.SystemTestUtil.initiatingConfig;
+import static uk.co.real_logic.artio.system_tests.SystemTestUtil.newInitiatingLibrary;
+
+public class ConnectionTimeoutOnFailedAcceptorSystemTest extends AbstractGatewayToGatewaySystemTest
+{
+    private DebugTcpChannelSupplier debugTcpChannelSupplier;
+
+    @Before
+    public void launch()
+    {
+        delete(ACCEPTOR_LOGS);
+
+        mediaDriver = launchMediaDriver();
+
+        delete(CLIENT_LOGS);
+        final EngineConfiguration initiatingConfig = initiatingConfig(libraryAeronPort);
+        initiatingConfig.channelSupplierFactory(config ->
+        {
+            debugTcpChannelSupplier = new DebugTcpChannelSupplier(config);
+            return debugTcpChannelSupplier;
+        });
+        initiatingEngine = FixEngine.launch(initiatingConfig);
+
+        initiatingLibrary = newInitiatingLibrary(libraryAeronPort, initiatingHandler);
+        testSystem = new TestSystem(initiatingLibrary);
+    }
+
+
+    @Test(timeout = 1000)
+    public void connectTimeOutOnBadEngine() throws Exception
+    {
+        debugTcpChannelSupplier.disable();
+
+        // First reply times out
+        final Reply<Session> firstConnectReply = completeInitiateSession();
+        testSystem.awaitReply(firstConnectReply);
+        assertEquals(Reply.State.TIMED_OUT, firstConnectReply.state());
+
+        debugTcpChannelSupplier.enable();
+        Thread t = new Thread(() -> {
+            try {
+                ServerSocket serverSocket = new ServerSocket(port);
+                while (true) {
+                    Socket s = serverSocket.accept();
+                }
+            } catch (Exception e) {
+
+            }
+        });
+        t.setDaemon(true);
+        t.start();
+        // First reply also times out
+        final Reply<Session> secondConnectReply = completeInitiateSession();
+        assertEquals(Reply.State.EXECUTING, secondConnectReply.state());
+        testSystem.awaitReply(secondConnectReply);
+        assertEquals(Reply.State.TIMED_OUT, secondConnectReply.state());
+    }
+
+    private Reply<Session> completeInitiateSession()
+    {
+        final SessionConfiguration config = SessionConfiguration.builder()
+            .address("localhost", port)
+            .credentials(USERNAME, PASSWORD)
+            .senderCompId(INITIATOR_ID)
+            .targetCompId(ACCEPTOR_ID)
+            .timeoutInMs(200)
+            .build();
+
+        final Reply<Session> firstConnectReply = initiatingLibrary.initiate(config);
+        return firstConnectReply;
+    }
+}
