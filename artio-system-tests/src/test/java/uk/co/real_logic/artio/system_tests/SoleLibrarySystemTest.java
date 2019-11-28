8429c4d531609ade26c0024f6af1ed614ecab755
Richard Warburton <richard.warburton@gmail.com>
2019-11-28 21:47:38 +0000

    Add in an initial test for sole library mode

---
+++ artio-system-tests/src/test/java/uk/co/real_logic/artio/system_tests/SoleLibrarySystemTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2019 Adaptive Financial Consulting Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.real_logic.artio.system_tests;
+
+import org.junit.Before;
+import org.junit.Test;
+import uk.co.real_logic.artio.engine.EngineConfiguration;
+import uk.co.real_logic.artio.engine.FixEngine;
+import uk.co.real_logic.artio.engine.InitialAcceptedSessionOwner;
+import uk.co.real_logic.artio.library.LibraryConfiguration;
+import uk.co.real_logic.artio.messages.SessionState;
+
+import static org.junit.Assert.*;
+import static uk.co.real_logic.artio.TestFixtures.launchMediaDriver;
+import static uk.co.real_logic.artio.system_tests.SystemTestUtil.*;
+
+public class SoleLibrarySystemTest extends AbstractGatewayToGatewaySystemTest
+{
+
+    @Before
+    public void launch()
+    {
+        mediaDriver = launchMediaDriver();
+
+        final EngineConfiguration acceptingConfig = acceptingConfig(port, ACCEPTOR_ID, INITIATOR_ID)
+            .deleteLogFileDirOnStart(true)
+            .initialAcceptedSessionOwner(InitialAcceptedSessionOwner.SOLE_LIBRARY);
+        acceptingEngine = FixEngine.launch(acceptingConfig);
+
+        initiatingEngine = launchInitiatingEngine(libraryAeronPort);
+
+        final LibraryConfiguration acceptingLibraryConfig = acceptingLibraryConfig(acceptingHandler);
+        acceptingLibrary = connect(acceptingLibraryConfig);
+        initiatingLibrary = newInitiatingLibrary(libraryAeronPort, initiatingHandler);
+        testSystem = new TestSystem(acceptingLibrary, initiatingLibrary);
+    }
+
+    @Test
+    public void shouldOnlyHandOffSessionToApplicationWhenConnected()
+    {
+        connectSessions();
+
+        acceptingSession = acceptingHandler.lastSession();
+        assertNotNull("should automatically receive the session upon logon In SOLE_LIBRARY mode",
+            acceptingSession);
+        assertEquals(SessionState.ACTIVE, acceptingSession.state());
+
+        assertFalse("should not receive session exists callback in sole library mode",
+            acceptingHandler.hasSeenSession());
+    }
+}
