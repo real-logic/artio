89c70c141a862aed4f0768d3226f62390da6854d
Richard Warburton <richard.warburton@gmail.com>
2021-04-14 17:36:26 +0100

    Add in utilities to dump out the state of the on disk session contexts / session ids file and replay indices

---
+++ artio-samples/src/main/java/uk/co/real_logic/artio/engine/framer/SessionContextDumper.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2021 Adaptive Financial Consulting Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.real_logic.artio.engine.framer;
+
+import uk.co.real_logic.artio.engine.MappedFile;
+import uk.co.real_logic.artio.engine.SessionInfo;
+import uk.co.real_logic.artio.session.SessionIdStrategy;
+
+import java.io.File;
+
+/**
+ * Prints out the state of a session contexts file
+ */
+public final class SessionContextDumper
+{
+    public static void main(final String[] args)
+    {
+        final String fileName = args[0];
+        final File bufferFile = new File(fileName);
+        final MappedFile mappedFile = MappedFile.map(bufferFile, (int)bufferFile.length());
+        final SessionContexts contexts = new SessionContexts(
+            mappedFile,
+            SessionIdStrategy.senderAndTarget(),
+            1,
+            Throwable::printStackTrace);
+
+        for (final SessionInfo sess : contexts.allSessions())
+        {
+            System.out.println(sess);
+        }
+    }
+}
