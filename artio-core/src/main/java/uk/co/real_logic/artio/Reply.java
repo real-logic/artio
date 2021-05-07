b0df951156ced75b915489a95e1fe4f6557ab927
Richard Warburton <richard.warburton@gmail.com>
2021-05-07 11:22:46 +0100

    Add Reply.await() that takes a FixLibrary parameter

--- artio-core/src/main/java/uk/co/real_logic/artio/Reply.java
+++ artio-core/src/main/java/uk/co/real_logic/artio/Reply.java
@@ -1,11 +1,15 @@
 package uk.co.real_logic.artio;
 
+import org.agrona.concurrent.IdleStrategy;
 import uk.co.real_logic.artio.engine.FixEngine;
+import uk.co.real_logic.artio.library.FixLibrary;
 
 import java.util.concurrent.locks.LockSupport;
 
 public interface Reply<T>
 {
+    int DEFAULT_POLL_FRAGMENT_LIMIT = 10;
+
     /**
      * Performs a blocking wait on a reply. This can be used for waiting for the reply of administrative operations
      * within Artio. This shouldn't be used on a normal duty cycle as it will stop you from performing different
@@ -21,6 +25,30 @@ public interface Reply<T>
         while (reply.isExecuting())
         {
             LockSupport.parkNanos(1_000_000);
+        }
+        return reply;
+    }
+
+    /**
+     * Performs a blocking wait on a reply, whilst polling a <code>FixLibrary</code>. This can be used for waiting for
+     * the reply of administrative operations within Artio on a thread that should normally also poll the FixLibrary.
+     * This shouldn't be used on a normal duty cycle as it will stop you from performing different operation
+     * concurrently, but can be convenient for administrative operations that you want to await.
+     *
+     * @param reply the reply to wait for.
+     * @param library the FixLibrary to poll whilst awaiting the reply.
+     * @param idleStrategy the {@link IdleStrategy} to use when polling.
+     * @param <T> the type of the object returned by the reply.
+     * @return the reply
+     */
+    static <T> Reply<T> await(
+        final Reply<T> reply,
+        final FixLibrary library,
+        final IdleStrategy idleStrategy)
+    {
+        while (reply.isExecuting())
+        {
+            idleStrategy.idle(library.poll(DEFAULT_POLL_FRAGMENT_LIMIT));
         }
         return reply;
     }
