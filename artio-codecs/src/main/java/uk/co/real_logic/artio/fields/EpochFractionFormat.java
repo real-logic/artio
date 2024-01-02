be5de8f05d74a653989d4a1ff23e38727877bf65
Dmitry Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2024-01-02 13:13:47 +0100

    Update copyright.

--- artio-codecs/src/main/java/uk/co/real_logic/artio/fields/EpochFractionFormat.java
+++ artio-codecs/src/main/java/uk/co/real_logic/artio/fields/EpochFractionFormat.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2023 Real Logic Limited., Monotonic ltd.
+ * Copyright 2015-2024 Real Logic Limited., Monotonic ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
